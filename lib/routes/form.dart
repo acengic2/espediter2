5824776f7f9e0ec3bdcbb1bb1b882f355ab1be57
jusuf-el <el.jusuf97@gmail.com>
2020-01-01 21:22:57 +0100

    Ispravljena validacija datuma i riješen problem sa 1970. godinom, ubačene crtice izmedju textfields za destinacije

--- lib/routes/form.dart
+++ lib/routes/form.dart
@@ -29,31 +29,56 @@ class _UserFormState extends State<InterdestinationForm> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(bottom: 2, left: 16.0, right: 16.0, top: 2),
+        margin: EdgeInsets.only(left: 18.0, right: 16.0),
         child: Row(children: <Widget>[
           Expanded(
               flex: 1,
-              child: Container(
-                margin: EdgeInsets.only(bottom: 10.0),
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: Icon(
-                        Icons.brightness_1,
-                        color: Color.fromRGBO(0, 0, 0, 0.5),
-                        size: 15.0,
-                      ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 0),
+                    height: 12,
+                    width: 0,
+                    decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Color.fromRGBO(0, 0, 0, 0.12))),
+                  ),
+                  Container(
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        Container(
+                            height: 20,
+                            width: 20,
+                            margin: EdgeInsets.only(right: 8, left: 16)),
+                        Container(
+                          height: 16,
+                          width: 16,
+                          child: Icon(
+                            Icons.brightness_1,
+                            color: Color.fromRGBO(0, 0, 0, 0.5),
+                            size: 10.0,
+                          ),
+                        ),
+                        Container(
+                          child: Icon(
+                            Icons.brightness_1,
+                            color: Color.fromRGBO(0, 0, 0, 0.2),
+                            size: 20.0,
+                          ),
+                        )
+                      ],
                     ),
-                    Container(
-                      child: Icon(
-                        Icons.brightness_1,
-                        color: Color.fromRGBO(0, 0, 0, 0.2),
-                        size: 30.0,
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 0),
+                    height: 12,
+                    width: 0,
+                    decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Color.fromRGBO(0, 0, 0, 0.12))),
+                  )
+                ],
               )),
           Expanded(
               flex: 9,
