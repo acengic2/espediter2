6da089432b0e5a6e7ae4504243b95daf5e6f4c9b
acengic2 <axc2629@g.rit.edu>
2020-01-06 14:39:26 +0100

    readMe.

--- lib/routes/inderdestination.dart
+++
@@ -1,5 +0,0 @@
-class Interdestination {
-  String interdestinationData;
-  Interdestination({this.interdestinationData = ''});
-}
-
--- lib/routes/random.dart
+++
@@ -1,6 +0,0 @@
-//import 'dart:math';
-
-main() {
-  //var rng = new Random();
-  
-}
